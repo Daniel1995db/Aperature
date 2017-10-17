5f4d74b52f6abd5329700b417b0ab7e3fe09e46d
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2017-10-17 17:59:20 -0400

    Sign up page, and profile page

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'carrierwave/orm/activerecord'
 require 'sqlite3'
 require './models'
 
+enable :sessions
 set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
 
 get '/' do
@@ -14,4 +15,33 @@ end
 
 get '/profile' do
 	@users = User.all
+	erb :profile
+end
+
+get'/login' do
+	erb :login
+end
+
+get '/signup' do 
+	erb :signup
+end
+
+post '/signup' do
+  user = User.new(
+    name: params[:name],
+    last: params[:last],
+    username: params[:username],
+    password: params[:password],
+    photo: params[:photo],
+    occupancy: params[:occupancy],
+    relationship: params[:relationship]
+  )
+  user.save
+  # if user.save
+  #   flash[:message] = "Welcome! You've registered successfully"
+  #   redirect '/'
+  # else
+  #   flash[:message] = "Ooops, something went wrong. Your account couldn't be created at this time"
+  #   redirect back
+  
 end
