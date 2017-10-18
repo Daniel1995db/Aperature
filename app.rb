79afaa3ede8d54e5e9591dd0b2f7a128545d9605
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2017-10-18 12:35:20 -0400

    fixed bug on sign up and profile

--- app.rb
+++ app.rb
@@ -9,6 +9,18 @@ require './models'
 enable :sessions
 set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
 
+CarrierWave.configure do |config|
+  config.root = File.dirname(__FILE__) + "/public"
+end
+
+before do
+  current_user
+end
+
+# login protecting routes
+before ['/buzzfeed','/profile','/newpost','/logout'] do
+  redirect '/' unless @current_user
+end
 
 get '/' do
 	erb :home
@@ -37,6 +49,7 @@ get '/signup' do
 end
 
 post '/signup' do
+  p params
   user = User.new(
     name: params[:name],
     last: params[:last],
@@ -46,8 +59,10 @@ post '/signup' do
     occupancy: params[:occupancy],
     relationship: params[:relationship]
   )
+  # user.photo = params[:photo]
   if user.save
     flash[:message] = "Welcome! You've registered successfully"
+    session[:user_id] = user.id
     redirect '/buzzfeed'
   else
     flash[:message] = "Ooops, something went wrong. Your account couldn't be created at this time"
@@ -58,12 +73,23 @@ end
 post '/login' do
   user = User.find_by(username: params[:username])
   if user && user.password == params[:password]
+    session[:user_id] = user.id
     flash[:message] = "Welcome"
     redirect '/buzzfeed'
   else 
     flash[:message] = "Incorrect Username or Password"
     redirect back 
   end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  flash[:message] = "You've Logged Out Safely"
+  redirect '/'
+end
+
+def current_user
+  @current_user = User.find(session[:user_id]) if session[:user_id]
 end
 
 
